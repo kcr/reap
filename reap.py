931822f0a2baba1d4cde66bc94696548113d3a68
Karl Ramm <kcr@1ts.org>
2014-12-25 16:59:05 -0500

    Jump and fork are now just skip, and are relative (+)
    (+ better debugging, better tests, tack an end-sigil on the end so we
    always run out of code on a match)

--- reap.py
+++ reap.py
@@ -1,33 +1,44 @@
 #!/usr/bin/python3
 
-def execute_backtrack(codelet, string, ip = 0):
+def execute_backtrack(codelet, string, ip = 0, level = 0):
     state = None
-    for i, c in enumerate(string):
+    for i, c in enumerate(string + '$'):#XXX need a better end sigil
         process = True
         while process:
+            if ip >= len(codelet): # ran off the end
+                dprint(level*' ', ip, '>=', len(codelet), '-> True')
+                return True
+
             f = codelet[ip]
-            print (ip, f, state, i, c)
+            dprint (level*' ', ip, f, state, i, c)
 
             action, rest = f[0], f[1:]
             if action == 'match':
                 if c != rest[0]:
+                    dprint(level*' ', ip, c,'!=', rest[0:], '-> False')
                     return False
                 process = False
-            elif action == 'fork':
-                for target in rest:
-                    if execute_backtrack(codelet, string[i:], target):
+            elif action == 'skip':
+                for target in rest[:-1]:
+                    if execute_backtrack(codelet, string[i:], ip + target, level + 1):
                         return True
-            elif action == 'jump':
-                (ip,) = rest
+                ip = ip + rest[-1]
+                continue
             else:
                 raise Exception('unknown action', action)
 
             ip += 1
-            if ip > len(codelet): # ran off the end
-                break
 
+    if ip < len(codelet):
+        dprint (level * ' ', ip, 'did not satisfy the pattern -> False')
+        # unconsumed
+        return False
+
+    dprint (level * ' ', ip, 'everythign consumed -> True')
     return True
 
+def dprint(*args, **kw):
+    return print(*args, **kw)
 
 def trycode(codelet, ostensible, string, expected):
     print('Trying', string,'against the ostensible', ostensible)
@@ -51,11 +62,11 @@ if __name__ == '__main__':
     trycode(codelet0, 'cat', 'dot', False)
 
     codelet1 = [
-        ('fork', 5),
+        ('skip', 1, 5),
         ('match', 'c'),
         ('match', 'a'),
         ('match', 't'),
-        ('jump', 8),
+        ('skip', 4),
         ('match', 'd'),
         ('match', 'o'),
         ('match', 'g'),
@@ -64,4 +75,6 @@ if __name__ == '__main__':
     trycode(codelet1, 'cat|dog', 'cat', True)
     trycode(codelet1, 'cat|dog', 'dog', True)
     trycode(codelet1, 'cat|dog', 'dot', False)
+    trycode(codelet1, 'cat|dog', 'catx', True)
+    trycode(codelet1, 'cat|dog', 'ca', False)
 

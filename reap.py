9b9541ba8283fdffb2624c63c53b6ea57651f87f
Karl Ramm <kcr@1ts.org>
2014-12-25 14:17:56 -0500

    eschew superfluous infrastructure

--- reap.py
+++ reap.py
@@ -1,72 +1,23 @@
 #!/usr/bin/python3
 
-FAILURE = 'failure'
-NEXT = 'next'
-FORK = 'fork'
-JUMP = 'jump'
-
-
-class Operation:
-    def __init__(self, *args):
-        self.args = args
-
-    def __call__(self, state, index, char):
-        return None, None
-
-    def __repr__(self):
-        return '%s(%s)' % (
-            self.__class__.__name__,
-            ', '.join(repr(a) for a in self.args),
-            )
-
-
-class Exactly(Operation):
-    def __init__(self, char):
-        super().__init__(char)
-        self.char = char
-
-    def __call__(self, state, index, char):
-        if char != self.char:
-            return FAILURE, ()
-        return NEXT, ()
-
-
-class Fork(Operation):
-    def __init__(self, ip):
-        super().__init__(ip)
-        self.ip = ip
-
-    def __call__(self, state, index, char):
-        return FORK, (self.ip,)
-
-
-class Jump(Operation):
-    def __init__(self, ip):
-        super().__init__(ip)
-        self.ip = ip
-
-    def __call__(self, state, index, char):
-        return JUMP, (self.ip,)
-
-
 def execute_backtrack(codelet, string, ip = 0):
     state = None
     for i, c in enumerate(string):
-        action = None
-        while action != NEXT:
+        process = True
+        while process:
             f = codelet[ip]
-            #print (ip, f, state, i, c)
-            action, rest = f(state, i, c)
-            #print (ip, action, rest)
-            if action == FAILURE:
-                return False
-            elif action == FORK:
+            print (ip, f, state, i, c)
+
+            action, rest = f[0], f[1:]
+            if action == 'match':
+                if c != rest[0]:
+                    return False
+                process = False
+            elif action == 'fork':
                 for target in rest:
                     if execute_backtrack(codelet, string[i:], target):
                         return True
-            elif action == NEXT:
-                pass
-            elif action == JUMP:
+            elif action == 'jump':
                 (ip,) = rest
             else:
                 raise Exception('unknown action', action)
@@ -76,6 +27,7 @@ def execute_backtrack(codelet, string, ip = 0):
                 break
 
     return True
+
 
 def trycode(codelet, ostensible, string, expected):
     print('Trying', string,'against the ostensible', ostensible)
@@ -89,9 +41,9 @@ def trycode(codelet, ostensible, string, expected):
 
 if __name__ == '__main__':
     codelet0 = [
-        Exactly('c'),
-        Exactly('a'),
-        Exactly('t'),
+        ('match', 'c'),
+        ('match', 'a'),
+        ('match', 't'),
         ]
 
     trycode(codelet0, 'cat', 'cat', True)
@@ -99,14 +51,14 @@ if __name__ == '__main__':
     trycode(codelet0, 'cat', 'dot', False)
 
     codelet1 = [
-        Fork(5),
-        Exactly('c'),
-        Exactly('a'),
-        Exactly('t'),
-        Jump(8),
-        Exactly('d'),
-        Exactly('o'),
-        Exactly('g'),
+        ('fork', 5),
+        ('match', 'c'),
+        ('match', 'a'),
+        ('match', 't'),
+        ('jump', 8),
+        ('match', 'd'),
+        ('match', 'o'),
+        ('match', 'g'),
         ]
 
     trycode(codelet1, 'cat|dog', 'cat', True)

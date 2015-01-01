7360ae948be68a2ae8d1f6dbec57b713af126ea2
Karl Ramm <kcr@1ts.org>
2014-12-31 19:10:28 -0500

    let's put the performance check in the repo

---
+++ perf.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+
+import contextlib
+import time
+import re
+
+import reap
+
+
+def match(re, string):
+    return reap.execute_threaded(reap.generate(reap.parse_2rp(re)), string)
+
+
+@contextlib.contextmanager
+def timer():
+    t0 = time.time()
+    yield
+    print('duration', time.time() - t0, 'seconds')
+
+
+def main():
+    for i in range(20):
+        tre = 'a?'*i + 'a'*i
+        s = 'a'*i
+        print()
+        print(i, repr(tre), 'against', repr(s))
+        print('re   ', end='')
+        with timer():
+            re.compile(tre).match(s)
+
+        print('reap ', end='')
+        with timer():
+            match(tre, s)
+
+
+if __name__ == '__main__':
+    main()
